4556c8b366e316d986c5c3a16c1f377db4a206c6
ernes <ernes@LAPTOP-PJ4CRS50>
2021-08-28 21:23:09 -0400

    Add project files.

---
+++ BinaryTree/BinaryTreeUtils.cs
@@ -0,0 +1,54 @@
+﻿using BalanceTreeCheck.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceTreeCheck.BinaryTree
+{
+
+    //                1
+    //            /      \
+    //           2        3
+    //          / \     /    \
+    //         4   5   6      7
+    //          \     / \    /  \
+    //           8   9   10  11  12
+    //              /
+    //             13
+    //
+    public class BinaryTreeUtils
+    {
+        public bool IsBinaryTreeBalanced<T>(BinaryTreeNode<T> root)
+        {
+            var treeHeight = GetBinaryTreeHeight(root);
+
+            return treeHeight >= 0;
+        }
+
+        private int GetBinaryTreeHeight<T>(BinaryTreeNode<T> node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            var hl = GetBinaryTreeHeight(node.Left);
+            var hr = GetBinaryTreeHeight(node.Right);
+
+            if (hl == -1 || hr == -1)
+            {
+                return -1;
+            }
+
+            if (Math.Abs(hl - hr) > 1)
+            {
+                return -1;
+            }
+
+            if (hl > hr) return hl + 1;
+            else return hr + 1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BalanceTreeCheck
+{
+    class Program
+    {
+        //                1
+        //            /      \
+        //           2        3
+        //          / \     /    \
+        //         4   5   6      7
+        //          \     / \    /  \
+        //           8   9   10  11  12
+        //              /
+        //             13
+        //
+
+        //                1
+        //            /      \
+        //           2        3
+        //          / \     /    \
+        //         4   5   6      7
+        //          \     / \    /  \
+        //           8   9   10  11  12
+        //              /
+        //             13
+        //
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
